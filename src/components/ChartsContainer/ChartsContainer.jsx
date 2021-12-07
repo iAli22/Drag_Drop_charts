1d9362acec35e6a416db0149289bcb8376e45770
iAli22 <ali_mahde22@yahoo.com>
2021-12-07 20:44:22 +0200

    init commit

--- src/components/ChartsContainer/ChartsContainer.jsx
+++ src/components/ChartsContainer/ChartsContainer.jsx
@@ -52,14 +52,6 @@ function ChartsContainer() {
         borderColor: "rgb(255, 99, 132)",
         backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
-
-      // ? if i want multi dimension
-      // {
-      //   label: "Dataset 2",
-      //   data: labels.map(() => [1]),
-      //   borderColor: "rgb(53, 162, 235)",
-      //   backgroundColor: "rgba(53, 162, 235, 0.5)"
-      // }
     ],
   };
 
